481d3aed82247ff1fb0e90dabd906cecca79dfba
Dr. Adanos Diego Flamel <adanos@itardis.intranet.stormcage.ru>
2017-07-05 10:26:45 +0300

    Неработоспособно. Бэкап адаптированного алгоритма.

--- src/graph.h
+++ src/graph.h
@@ -4,6 +4,16 @@
 
 #pragma once
 
+/* Количество узлов */
+#ifndef MATRIX_SIZE
+    #define MATRIX_SIZE 5
+#endif
+
+/* Начало именования узлов */
+#ifndef NODE_NAME_START
+    #define NODE_NAME_START 'A'
+#endif
+
 #include <stdlib.h>
 #include <iostream>
 #include <iomanip>
@@ -18,11 +28,12 @@ class graph {
     public:
         graph () { 
             for (int i = 0; i < MATRIX_SIZE; ++i)
-                for (int j = 0; j < MATRIX_SIZE; j++)
+                for (int j = 0; j < MATRIX_SIZE; ++j)
                     matrix[i][j] = 0;
         };
         ~graph () {};
         void generate ();
+        void dejkstra (char nodeName);
         void show ();
 
 };
@@ -31,7 +42,7 @@ class graph {
 void graph :: generate () {
 
     for (int i=0; i<MATRIX_SIZE; ++i)
-        for (int j=0; j<MATRIX_SIZE; j++) {
+        for (int j=0; j<MATRIX_SIZE; ++j) {
             if ((i != j) && (rand() % 2))
                 matrix[i][j] = rand() % 100;
         }
@@ -47,13 +58,13 @@ void graph :: show () {
 
     /* Выводим заголовок матрицы смежности */
     for (int i=0; i<MATRIX_SIZE; ++i)
-        std::cout << setw(2) << (sym = 'A'+i) << " ";
+        std::cout << setw(2) << (sym = NODE_NAME_START+i) << " ";
     std::cout << endl;
 
     /* Выводим матрицу смежности*/
     for (int i=0; i<MATRIX_SIZE; ++i) {
         // Левый столбец букв
-        std::cout << setw(2) << (sym = 'A'+i) << " ";
+        std::cout << setw(2) << (sym = NODE_NAME_START+i) << " ";
 
         for (int j=0; j<MATRIX_SIZE; j++)
             std::cout << setw(2) << matrix[i][j] << " ";
@@ -64,6 +75,63 @@ void graph :: show () {
 
 }
 
+/* Функция применения алгоритма Дейкстры к графу */
+void graph :: dejkstra (char nodeName) {
+
+    int nodeDistance[MATRIX_SIZE];
+    
+    nodeName=nodeName-NODE_NAME_START; // Приводим имя узла к числу
+    nodeDistance[nodeName] = 0;        // Кратчайшее расстояние до самого себя = 0
+
+    /* Массив флагов найденности расстояний до вершин */
+    bool used[MATRIX_SIZE];
+    for (int i = 0; i < MATRIX_SIZE; ++i)
+        used[i] = false;
+
+    /* Для каждой вершины орграфа */
+    for (int iteration = 0; iteration < MATRIX_SIZE; ++iteration) {
+
+        int node = -1;              // Номер обрабатываемой вершины
+        int nodeDistanceV = 16383;  // Расстояние по умолчанию
+
+        /* Выбираем вершину, кратчайшее расстояние до которой еще не найдено */
+        for (int i = 0; i < MATRIX_SIZE; ++i) {
+
+            if (used[i])
+                continue;
+
+            if (nodeDistanceV < nodeDistance[i])
+                continue;
+
+            node = i;
+            nodeDistanceV = nodeDistance[i];
+        }
+
+        /* Для всех ребёр */
+        for (int i = 0; i < matrix[node][MATRIX_SIZE]; ++i) {
+
+            if (matrix[node][i] != 0) {
+
+                int nodeTwo = i;
+                int nodeTwoWeight = matrix[node][i];
+
+                if (nodeDistance[node] + nodeTwoWeight < nodeDistance[nodeTwo]) {
+                    nodeDistance[nodeTwo] = nodeDistance[node] + nodeTwoWeight;
+                }
+                
+            }
+        }
+
+        // Помечаем вершину v просмотренной, до нее найдено кратчайшее расстояние
+        used[node] = true;
+
+    }
+    
+    cout <<"Result: \n\n";
+    for (int i=0;i<MATRIX_SIZE;++i)
+        cout << nodeDistance[i] << " ";
+    
+}
 
 //void GR :: Make(int G[ MaxV ][ MaxV ]) { //матрица → списки смежности
 //    int ls = 0, f;
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,11 @@
  * Задание: Подсчёт расстояний от произвольной вершины до всех остальных вершин в ориентированном ненагруженном графе
  ----- */
 
+/* Количество узлов */
 #define MATRIX_SIZE 5
+
+/* Начало именования узлов */
+#define NODE_NAME_START 'a'
 
 #include <iostream>
 #include <time.h>
@@ -16,6 +20,8 @@ int main (int argc, char **argv) {
 
     srand(time(NULL));
 
+    char target;
+
     /* Генерация случайного графа */
     graph processGraph;
     processGraph.generate();
@@ -23,6 +29,19 @@ int main (int argc, char **argv) {
     /* Вывод матрицы смежности графа на экран */
     cout << "\n Generated adjacency matrix:";
     processGraph.show();
+    
+    /* Ввод имени узла для расчёта */
+    cout << "\n Input node name to dejkstra: ";
+    cin >> target;
+    
+    /* Проверка имени узла*/
+    if ((target-NODE_NAME_START >= MATRIX_SIZE) || (target - NODE_NAME_START < 0)) {
+        cout << " Given node does not exist. Will now exit. \n";
+        return 1;
+    }
+    cout << " Processing node " << target << ".\n" << endl;
+    
+    processGraph.dejkstra(target);
     
     std::cout << "\n Press enter to continue... "; 
     std::cin.get(); 
